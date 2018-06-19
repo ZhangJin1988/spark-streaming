d433316ae8aa7b4467c025f5548e9e8fe5079942
ZhangJin1988 <315552364@qq.com>
2018-06-19 16:32:22 +0800

    kafka practice

---
+++ src/main/java/cn/kafka/api/ConsumerApi.java
@@ -0,0 +1,41 @@
+package cn.kafka.api;
+
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * @author zhangjin
+ * @create 2018-06-19 16:05
+ */
+public class ConsumerApi {
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("bootstrap.servers", "hdp1:9092,hdp2:9092,hdp3:9092");
+        properties.setProperty("group.id", "c9527");
+        /**
+         *
+         *latest ： 最新的    一开始 就消费最新的数据
+         *earliest ： 最早的  一上来就去消费 最早的消息
+         *none ：
+         */
+        properties.setProperty("auto.offset.reset", "earliest");// latest
+        //自动提交偏移量
+        properties.setProperty("enable.auto.commit", "true");//_consumer_offset 特殊的主题
+
+        /**
+         * 消费者 客户端
+         */
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+        /**
+         *制定你要消费的主题 从哪个主题消费数据 可以同时指定多个主题
+         */
+        consumer.subscribe(Arrays.asList("kafka"));
+
+    }
+}
--- src/main/java/cn/kafka/api/ProducerApi.java
+++ src/main/java/cn/kafka/api/ProducerApi.java
@@ -1,10 +1,14 @@
 package cn.kafka.api;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.junit.Test;
 
-import java.util.Properties;
+import java.util.HashMap;
+import java.util.UUID;
+import java.util.concurrent.Future;
 
 /**
  * @author zhangjin
@@ -12,23 +16,60 @@ import java.util.Properties;
  */
 public class ProducerApi {
 
-    public static void main(String[] args) {
+
+    @Test
+    public void test1() throws InterruptedException {
 
 
-        Properties props = new Properties();
-        props.put("bootstrap.servers", "hdp1:9092");
-        props.put("acks", "all");
-        props.put("retries", 0);
-        props.put("batch.size", 16384);
-        props.put("linger.ms", 1);
-        props.put("buffer.memory", 33554432);
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    }
 
-        Producer<String, String> producer = new KafkaProducer<>(props);
-        for (int i = 0; i < 100; i++)
-            producer.send(new ProducerRecord<>("topic1", Integer.toString(i), Integer.toString(i)));
+    public static void main(String[] args) throws InterruptedException {
 
-        producer.close();
+
+        //封装producer的参数的
+        HashMap<String, Object> configs = new HashMap<>();
+        //kafka集群所在的位置
+        configs.put("bootstrap.servers", "hdp1:9092,hdp2:9092,hdp3:9092");
+        //key 和 value的序列化类
+        configs.put("key.serializer", StringSerializer.class);
+        configs.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        //
+        /**
+         * acks 应答参数设置
+         *  all| -1 : follower--》 leader --》 producer
+         *  0: 不搭理 leader收到消息后 不做任何响应
+         *  1:leader收到消息后 会给producer应答
+         */
+        configs.put("acks", "1");
+
+
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String, String>(configs);
+
+        int i = 0;
+        while (true) {
+            Thread.sleep(100l);
+            //将消息封装成一个producerRecord
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>("hellokafka", UUID.randomUUID().toString(),"this is message" + i);
+            Future<RecordMetadata> send = kafkaProducer.send(producerRecord);
+            System.out.println("this is message " + i);
+            i++;
+        }
+
+//
+//        Properties props = new Properties();
+//        props.put("bootstrap.servers", "hdp1:9092");
+//        props.put("acks", "all");
+//        props.put("retries", 0);
+//        props.put("batch.size", 16384);
+//        props.put("linger.ms", 1);
+//        props.put("buffer.memory", 33554432);
+//        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+//        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+//
+//        Producer<String, String> producer = new KafkaProducer<>(props);
+//        for (int i = 0; i < 100; i++)
+//            producer.send(new ProducerRecord<>("topic1", Integer.toString(i), Integer.toString(i)));
+//
+//        producer.close();
     }
 }
